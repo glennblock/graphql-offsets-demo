a9f33c9f815d9bb64e0badb2ba650891c4cd0db8
Glenn Block <glenn.block@gmail.com>
2020-09-07 10:56:46 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# graphql-offset-demo
+# Overview
+This demo illustates how to implement offsets in your Apollo GraphQL endpoint in order to support paging.
+
+# Disclaimer
+This is purely for educational purposes. The app is not secure, the data store is in memory, and the approach used here won't scale. The concepts here can easily be adapted to a real world system. 
+
+# Installing
+- Clone the repo. 
+- Run `npm install` (or use yarn) in the folder. 
+
+# Running
+- `npm start`
+- After the server is running open "http://localhost:4000"
+- The graphiql editor will open. 
+
+# Walkthrough
+
+## Query 1 - Retrieving the first set of results
+Issue the following query to get the first set of results
+```
+{
+  books( 
+    limit:3
+  ) {
+      title
+      author
+      description
+  }
+}
+```
+![query-1](https://user-images.githubusercontent.com/141124/92410048-a855b900-f0f7-11ea-8c04-7a5421730b7e.png)
+
+## Query 2 - Getting the next set of results
+Using the `offset` param you can get the next page of results
+```
+{
+  books( 
+    limit:3,
+    offset:3
+  ) {
+      title
+      author
+      description
+  }
+}
+```
+![query-2](https://user-images.githubusercontent.com/141124/92410297-8872c500-f0f8-11ea-8df8-73353b3c2601.png)
---
+++ index.js
@@ -0,0 +1,49 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { readFileSync } = require('fs');
+const { brotliCompressSync } = require('zlib');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+    description: String
+    published: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books(offset: Int, limit: Int): [Book]
+  }
+`;
+
+//from https://gist.githubusercontent.com/nanotaboada/6396437/raw/82dca67cc3b6a5ccfcf8af012664cdaa0025d999/books.json
+var books = JSON.parse(readFileSync('books.json')).books;
+books.sort((a,b)=>a.title.localeCompare(b.title));
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: (parent, args, context, info) => {
+      return books.slice(args.offset, args.offset + args.limit);
+    }
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
